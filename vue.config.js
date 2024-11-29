4f40843b3db0c1abce3908935a3545addb4bf231
shinssuji <01029280981@hanmail.net>
2024-11-29 22:14:33 +0900

    css, scss 수정

--- vue.config.js
+++ vue.config.js
@@ -1,34 +1,34 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: '/works',
+  // publicPath: '/works',
   // 정적 자산 경로 설정
   assetsDir: 'assets',
 
   // Webpack 설정 커스터마이징
-  chainWebpack: (config) => {
-    config.output
-      .filename('js/[name].js')
-      .chunkFilename('js/[name].js')
-      .end();
+  // chainWebpack: (config) => {
+  //   config.output
+  //     .filename('js/[name].js')
+  //     .chunkFilename('js/[name].js')
+  //     .end();
 
-    // CSS와 이미지 경로 문제 해결
-    config.plugin('html').tap((args) => {
-      args[0].minify = false; // 경로에서 최적화로 인한 의도치 않은 문제 방지
-      return args;
-    });
-  },
+  //   // CSS와 이미지 경로 문제 해결
+  //   config.plugin('html').tap((args) => {
+  //     args[0].minify = false; // 경로에서 최적화로 인한 의도치 않은 문제 방지
+  //     return args;
+  //   });
+  // },
 
-  configureWebpack: {
-    output: {
-      publicPath: './', // 모든 정적 파일 경로가 상대경로로 설정
-    },
-  },
+  // configureWebpack: {
+  //   output: {
+  //     publicPath: './', // 모든 정적 파일 경로가 상대경로로 설정
+  //   },
+  // },
 
-  // CSS 및 이미지 경로의 상대 경로 유지
-  css: {
-    extract: {
-      publicPath: './',
-    },
-  },
+  // // CSS 및 이미지 경로의 상대 경로 유지
+  // css: {
+  //   extract: {
+  //     publicPath: './',
+  //   },
+  // },
 })
